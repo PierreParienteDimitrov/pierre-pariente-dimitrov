77e71bd79370b954e385e157c8fdfc56a720d7e4
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-02-02 20:02:03 +0100

    created layout with navbar and footer empty components

---
+++ components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+	return (
+		<div>
+			<h1>I am the navbar</h1>
+		</div>
+	);
+};
+
+export default Nav;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const about = () => {
+	return (
+		<div>
+			<h1>I am the about page</h1>
+		</div>
+	);
+};
+
+export default about;
